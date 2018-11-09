3c6606553077dd045b12df62f9d0e1a4880c10df
Killian Murphy <killian.murphy@york.ac.uk>
2018-11-09 10:24:43 +0000

    Update control script to spawn reader threads for each attached instrument

--- control.py
+++ control.py
@@ -5,8 +5,8 @@ AQ INSTRUMENT CONTROL
 ============================================================================="""
 import queue
 
-import data_reader
-import data_writer
+import data_reader as dr
+import data_writer as dw
 #===============================================================================
 def main():
     """
@@ -14,12 +14,19 @@ def main():
     """
     global_queue = queue.Queue()
 
-    reader_thread = data_reader.DataReader('reader_1',
-                                           '/dev/cu.usbmodem1411',
-                                           global_queue)
-    writer_thread = data_writer.DataWriter('writer_1', global_queue)
+    reader_threads = (dr.DataReader('sensor_array_reader_1',
+                                    '/dev/SENSOR_ARRAY_1',
+                                    global_queue),
+                      dr.DataReader('sensor_array_reader_2',
+                                    '/dev/SENSOR_ARRAY_2',
+                                    global_queue),
+                      dr.DataReader('pm_reader',
+                                    '/dev/PM_INSTRUMENT',
+                                    global_queue))
+    writer_thread = dw.DataWriter('writer_1', global_queue)
 
-    reader_thread.start()
+    for thread in reader_threads:
+        thread.start()
     writer_thread.start()
 #===============================================================================
 if __name__ == '__main__':
