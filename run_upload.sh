5d51a14a5a4706db9e016b55fbf7096683f60e72
Killian Murphy <killian.murphy@york.ac.uk>
2019-02-13 12:46:07 +0000

    Change to working directory when running Python scripts

--- run_upload.sh
+++ run_upload.sh
@@ -10,6 +10,7 @@ LOGFILE="${LOGDIR}/upload.log"
 PYTHON="/opt/python/3.7.1/bin/python3"
 SITE="aviva"
 
+cd "${WORKDIR}" || exit 1
 for i in {1..2}; do
   ${PYTHON} "${WORKDIR}/upload.py" "${SITE}" "${LOGDIR}/SENSOR_ARRAY_${i}" >> "${LOGFILE}" 2>&1
 done;
