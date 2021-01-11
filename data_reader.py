5e7ada50e52a316a3d6214f2c1a2c0613ca8205e
Killian Murphy <killian.murphy@york.ac.uk>
2021-01-11 16:39:06 +0000

    Add PurpleAir to device list and attempt to read/write its data

--- data_reader.py
+++ data_reader.py
@@ -10,54 +10,70 @@ import threading
 import serial
 from serial import serialutil
 from serial.tools.list_ports import comports
-#===============================================================================
+
+# ===============================================================================
 class DataReader(threading.Thread):
     """
     DataReader describes a thread whose purpose is to read lines of serial input
     from a specified serial port and enqueue said lines of serial input into a
     shared FIFO queue.
     """
+
     def __init__(self, name, port_name, shared_queue):
         threading.Thread.__init__(self)
-        err_string = (f"[{dt.now().__str__()}] INFO: DataReader {name} INITIALISING WITH PORT NAME"
-                      f" {port_name}\n")
+        err_string = (
+            f"[{dt.now().__str__()}] INFO: DataReader {name} INITIALISING WITH PORT NAME"
+            f" {port_name}\n"
+        )
         sys.stderr.write(err_string)
         self.name = name
         self.port_name = port_name
         self.queue = shared_queue
 
         try:
-            info_string = (f"[{dt.now().__str__()}] INFO: DataReader {self.name} CHECKING PORT "
-                           f"{self.port_name} AVAILABILITY\n")
+            info_string = (
+                f"[{dt.now().__str__()}] INFO: DataReader {self.name} CHECKING PORT "
+                f"{self.port_name} AVAILABILITY\n"
+            )
             sys.stderr.write(info_string)
             self.check_port_available()
         except serialutil.SerialException:
             # TODO: HANDLE PORT UNAVAILABLE
-            err_string = (f"[{dt.now().__str__()}] ERROR: DataReader {self.name} THINKS PORT "
-                          f"{self.port_name} IS UNAVAILABLE\n")
+            err_string = (
+                f"[{dt.now().__str__()}] ERROR: DataReader {self.name} THINKS PORT "
+                f"{self.port_name} IS UNAVAILABLE\n"
+            )
             sys.stderr.write(err_string)
 
         try:
-            info_string = (f"[{dt.now().__str__()}] INFO: DataReader {self.name} CHECKING PORT "
-                           f"{self.port_name} FUNCTIONALITY\n")
+            info_string = (
+                f"[{dt.now().__str__()}] INFO: DataReader {self.name} CHECKING PORT "
+                f"{self.port_name} FUNCTIONALITY\n"
+            )
             sys.stderr.write(info_string)
             self.check_port_function()
         except serialutil.SerialException:
             # TODO: HANDLE INCORRECT PORT FUNCTION
-            err_string = (f"[{dt.now().__str__()}] ERROR: DataReader {self.name} THINKS PORT "
-                          f"{self.port_name} IS MALFUNCTIONING\n")
+            err_string = (
+                f"[{dt.now().__str__()}] ERROR: DataReader {self.name} THINKS PORT "
+                f"{self.port_name} IS MALFUNCTIONING\n"
+            )
             sys.stderr.write(err_string)
 
         try:
-            info_string = (f"[{dt.now().__str__()}] INFO: DataReader {self.name} OPENING PORT "
-                           f"{self.port_name} FOR READ\n")
+            info_string = (
+                f"[{dt.now().__str__()}] INFO: DataReader {self.name} OPENING PORT "
+                f"{self.port_name} FOR READ\n"
+            )
             sys.stderr.write(info_string)
             self.port = serial.Serial(self.port_name, 9600, timeout=1)
             self.port.reset_input_buffer()
         except serialutil.SerialException:
             # TODO: HANDLE PORT NOT OPENABLE EXCEPTION
-            err_string = (f"[{dt.now().__str__()}] ERROR: DataReader {self.name} CAN'T OPEN "
-                          f"{self.port_name} FOR READ\n")
+            err_string = (
+                f"[{dt.now().__str__()}] ERROR: DataReader {self.name} CAN'T OPEN "
+                f"{self.port_name} FOR READ\n"
+            )
             sys.stderr.write(err_string)
 
     def check_port_available(self):
@@ -86,8 +102,10 @@ class DataReader(threading.Thread):
         """
         Read and return a line of attached instrument data.
         """
-        info_string = (f"[{dt.now().__str__()}] INFO: DataReader {self.name} READING DATA FROM "
-                       f"{self.port_name}\n")
+        info_string = (
+            f"[{dt.now().__str__()}] INFO: DataReader {self.name} READING DATA FROM "
+            f"{self.port_name}\n"
+        )
         sys.stderr.write(info_string)
         data = self.port.readline()
         return data
@@ -97,10 +115,14 @@ class DataReader(threading.Thread):
         Put an incoming line of data into a shared queue, ready for a DataWriter
         to process.
         """
-        info_string = (f"[{dt.now().__str__()}] INFO: DataReader {self.name} ENQUEUEING DATA TO "
-                       "SHARED QUEUE\n")
+        info_string = (
+            f"[{dt.now().__str__()}] INFO: DataReader {self.name} ENQUEUEING DATA TO "
+            "SHARED QUEUE\n"
+        )
         sys.stderr.write(info_string)
-        sys.stderr.write(f"[{dt.now().__str__()}] INFO: QUEUE IS NOW SIZE {self.queue.qsize()}\n")
+        sys.stderr.write(
+            f"[{dt.now().__str__()}] INFO: QUEUE IS NOW SIZE {self.queue.qsize()}\n"
+        )
         self.queue.put(f"{self.port_name[5:]},{data.decode()}", block=True)
 
     def run(self):
@@ -110,8 +132,10 @@ class DataReader(threading.Thread):
         while True:
             try:
                 self.enqueue_data(self.read_data_line())
-            except UnicodeDecodeError as exception:
-                err_string = (f"[{dt.now().__str__()}] INFO: DataReader {self.name} CAUGHT SOME "
-                              "GARBAGE; IGNORING DATA LINE\n")
+            except UnicodeDecodeError:
+                err_string = (
+                    f"[{dt.now().__str__()}] INFO: DataReader {self.name} CAUGHT SOME "
+                    "GARBAGE; IGNORING DATA LINE\n"
+                )
                 sys.stderr.write(err_string)
                 continue
--- data_writer.py
+++ data_writer.py
@@ -8,16 +8,20 @@ import os
 import re
 import sys
 import threading
-#===============================================================================
+
+# ===============================================================================
 class DataWriter(threading.Thread):
     """
     DataWriter describes a thread whose purpose is to flush lines of serial
     input from a shared FIFO queue into different files, depending on the nature
     of the serial input to be flushed.
     """
+
     def __init__(self, name, shared_queue, instrument_names):
         threading.Thread.__init__(self)
-        sys.stderr.write(f"[{dt.now().__str__()}] INFO: DataWriter {name} INITIALISING\n")
+        sys.stderr.write(
+            f"[{dt.now().__str__()}] INFO: DataWriter {name} INITIALISING\n"
+        )
         self.name = name
         self.queue = shared_queue
         create_log_directories(instrument_names)
@@ -28,8 +32,12 @@ class DataWriter(threading.Thread):
         """
         # TODO: MODIFY CALL TO queue.get TO INCORPORATE A SUITABLE TIMEOUT,
         # BASED ON ATTACHED INSTRUMENT RESPONSE TIMES
-        sys.stderr.write(f"[{dt.now().__str__()}] INFO: DataWriter {self.name} DEQUEUEING DATA\n")
-        sys.stderr.write(f"[{dt.now().__str__()}] INFO: QUEUE SIZE IS NOW {self.queue.qsize()}\n")
+        sys.stderr.write(
+            f"[{dt.now().__str__()}] INFO: DataWriter {self.name} DEQUEUEING DATA\n"
+        )
+        sys.stderr.write(
+            f"[{dt.now().__str__()}] INFO: QUEUE SIZE IS NOW {self.queue.qsize()}\n"
+        )
         return self.queue.get(block=True, timeout=None)
 
     def write_data(self, data):
@@ -37,31 +45,42 @@ class DataWriter(threading.Thread):
         Write data to the appropriate log file, named by instrument name and
         date.
         """
-        info_string = (f"[{dt.now().__str__()}] INFO: DataWriter {self.name} WRITING DATA TO LOG FILE"
-                       "\n")
+        info_string = (
+            f"[{dt.now().__str__()}] INFO: DataWriter {self.name} WRITING DATA TO LOG FILE"
+            "\n"
+        )
         sys.stderr.write(info_string)
         try:
-            data_fields = data.split(',')
-            id_string = (data_fields[0])
-            if re.match('ERROR', data_fields[1]):
+            data_fields = data.split(",")
+            id_string = data_fields[0]
+            if re.match("ERROR", data_fields[1]):
                 filename = f"{id_string}_error.log"
-                with open(f"logs/{id_string}/{filename}", 'a') as data_log:
+                with open(f"logs/{id_string}/{filename}", "a") as data_log:
                     data_log.write(data_fields[1])
-            else:
+            elif re.match("SENSOR_ARRAY_[AB]", data_fields[0]):
                 date = dt.utcfromtimestamp(int(data_fields[1]))
-                date_string = (f'{date.year}-{str(date.month).zfill(2)}-'
-                               f'{str(date.day).zfill(2)}')
-                filename = f"{id_string}_{date_string}_data.log"
-                with open(f"logs/{id_string}/{filename}", 'a') as data_log:
-                    data_log.write(','.join(data_fields[1:]))
+                date_string = (
+                    f"{date.year}-{str(date.month).zfill(2)}-"
+                    f"{str(date.day).zfill(2)}"
+                )
+            else:
+                date = dt.now()
+
+            filename = f"{id_string}_{date_string}_data.log"
+            with open(f"logs/{id_string}/{filename}", "a") as data_log:
+                data_log.write(",".join(data_fields[1:]))
         except OSError:
             # TODO: HANDLE INABILITY TO OPEN DATA LOG
-            err_string = (f"[{dt.now().__str__()}] ERROR: DataWriter {self.name} UNABLE TO APPEND TO "
-                          "DATA LOG\n")
+            err_string = (
+                f"[{dt.now().__str__()}] ERROR: DataWriter {self.name} UNABLE TO APPEND TO "
+                "DATA LOG\n"
+            )
             sys.stderr.write(err_string)
         except ValueError:
-            err_string = (f"[{dt.now().__str__()}] ERROR: UNABLE TO DECODE DATE FROM INSTRUMENT "
-                          "TIMESTAMP\n") 
+            err_string = (
+                f"[{dt.now().__str__()}] ERROR: UNABLE TO DECODE DATE FROM INSTRUMENT "
+                "TIMESTAMP\n"
+            )
             sys.stderr.write(err_string)
 
     def run(self):
@@ -71,20 +90,25 @@ class DataWriter(threading.Thread):
         while True:
             self.write_data(self.dequeue_data())
 
+
 def create_log_directories(instrument_names):
     """
     Create a directory to store instrument log files.
     """
     for instrument_name in instrument_names:
-        log_directory = f'logs/{instrument_name}'
+        log_directory = f"logs/{instrument_name}"
         if not os.path.isdir(log_directory):
-            info_string = (f'[{dt.now().__str__()}] INFO: LOG DIRECTORY FOR INSTRUMENT {instrument_name}'
-                           ' DOES NOT EXIST - CREATING\n')
+            info_string = (
+                f"[{dt.now().__str__()}] INFO: LOG DIRECTORY FOR INSTRUMENT {instrument_name}"
+                " DOES NOT EXIST - CREATING\n"
+            )
             sys.stderr.write(info_string)
             try:
                 os.makedirs(log_directory)
             except OSError:
                 # TODO: HANDLE NOT BEING ABLE TO CREATE LOG FILE
-                err_string = ('[{dt.now().__str__()}] ERROR: UNABLE TO CREATE LOG DIRECTORY FOR'
-                              f' INSTRUMENT {instrument_name}\n')
+                err_string = (
+                    "[{dt.now().__str__()}] ERROR: UNABLE TO CREATE LOG DIRECTORY FOR"
+                    f" INSTRUMENT {instrument_name}\n"
+                )
                 sys.stderr.write(err_string)
