03b08acfc85fde470eea4f4947636148a4331be3
Stuart Lacy <stuart.lacy@york.ac.uk>
2021-11-12 12:33:23 +0000

    Added logging to archive module and minor refactor

--- archive.py
+++ archive.py
@@ -3,12 +3,16 @@
 Archives yesterday's log file.
 ================================================================================
 #############################################################################"""
+import bocs_logger
+
 import argparse
 import datetime
 import glob
+import logging
 import os
 import zipfile
 import sys
+
 
 # ===============================================================================
 import boto3
@@ -17,6 +21,12 @@ import boto3
 def get_script_args():
     """
     Get command line arguments and options.
+
+    Args:
+        None.
+
+    Returns:
+        A argparse.Namespace object.
     """
     description = "Archives previous day's data file"
     arg_parser = argparse.ArgumentParser(description=description)
@@ -27,7 +37,7 @@ def get_script_args():
     return arg_parser.parse_args()
 
 
-def prepend_header(data_fn: str, header_fn: str):
+def prepend_header(data_fn: str, header_fn: str) -> None:
     """
     Adds the header to the data file.
 
@@ -42,8 +52,12 @@ def prepend_header(data_fn: str, header_fn: str):
     with open(data_fn, "r") as data_file:
         contents: list = data_file.readlines()
 
-    with open(header_fn, "r") as header_file:
-        header: list = header_file.readlines()
+    try:
+        with open(header_fn, "r") as header_file:
+            header: list = header_file.readlines()
+    except FileNotFoundError as ex:
+        error_string = f"Couldn't open header file at {header_fn}"
+        raise RuntimeError(error_string) from ex
 
     contents = header + contents
 
@@ -62,20 +76,30 @@ def compress_file(filename: str) -> str:
         The resultant archive filename.
     """
     outfile_name = f"{filename}.zip"
-    with zipfile.ZipFile(outfile_name, "w") as outzip:
-        outzip.write(
-            filename,
-            compress_type=zipfile.ZIP_DEFLATED,
-            arcname=os.path.basename(filename),
-        )
+    try:
+        with zipfile.ZipFile(outfile_name, "w") as outzip:
+            outzip.write(
+                filename,
+                compress_type=zipfile.ZIP_DEFLATED,
+                arcname=os.path.basename(filename),
+            )
+    except zipfile.BadZipFile as ex:
+        error_string = "Error when zipping file"
+        raise RuntimeError(error_string) from ex
 
     return outfile_name
 
 
-def file_to_archive(directory):
+def file_to_archive(directory: str) -> str:
     """
     Get the file to be archived, looking for yesterday's date in the
     filename and a .log extension.
+
+    Args:
+        - directory (str): The directory where the log should be located.
+
+    Returns:
+        The filename of the log to be archived.
     """
     absolute_directory = os.path.abspath(directory)
     yesterday = datetime.date.today() - datetime.timedelta(days=1)
@@ -84,11 +108,11 @@ def file_to_archive(directory):
     candidate = glob.glob(glob_pattern)
 
     if not candidate:
-        error_string = "ERROR: UNABLE TO FIND YESTERDAY'S DATA FILE!\n"
+        error_string = f"Unable to find a data file matching yesterday's date ({date_string})"
         raise RuntimeError(error_string)
 
     if len(candidate) > 1:
-        error_string = "ERROR: MULTIPLE DATA FILES FOUND FOR YESTERDAY!\n"
+        error_string = f"Multiple data files found for yesterday: {candidate}"
         raise RuntimeError(error_string)
 
     return candidate[0]
@@ -102,16 +126,21 @@ def main():
     script_args = get_script_args()
 
     try:
+        logging.info(
+            f"Attempting to archive yesterday's data file from directory {script_args.data_directory}"
+        )
         data_file = file_to_archive(script_args.data_directory)
+        prepend_header(data_file, "misc/header.txt")
+        compress_file(data_file)
     except RuntimeError as exception:
-        sys.stderr.write(str(exception))
-        sys.exit(1)
+        logging.error(exception)
+        logging.error("Terminating execution")
+        ret_code = 1
+    else:
+        logging.info("Data log archived successfully.")
+        ret_code = 0
 
-    prepend_header(data_file, "misc/header.txt")
-
-    compressed_data_file = compress_file(data_file)
-
-    sys.exit(0)
+    sys.exit(ret_code)
 
 
 # ===============================================================================
--- control.py
+++ control.py
@@ -6,11 +6,11 @@ AQ INSTRUMENT CONTROL
 - Starts a data writing thread which takes data from the shared data structure
   and writes it to log files
 ============================================================================="""
-import queue
 import bocs_logger
-
 import data_reader as dr
 import data_writer as dw
+
+import queue
 
 # ===============================================================================
 def main():
