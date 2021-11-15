693918a6c873320932967299a73106adaedb37d6
Stuart Lacy <stuart.lacy@york.ac.uk>
2021-11-15 15:59:39 +0000

    Removed unused logging setup file
    The logging is now setup in `bocs_control.__init__.py`, which means that
    any program that imports any file from `bocs_control` will setup the
    logging in the same way without needing to explicitly do so.

--- bocs_control/bocs_logger.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-logging.basicConfig(
-    format="[%(asctime)s.%(msecs)03d] %(levelname)s: %(filename)s:%(lineno)d %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-    level=logging.INFO,
-    style="%",
-)
