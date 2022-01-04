5b097b5bfaeada5e2d04b167b3d360327aa3abcc
Stuart Lacy <stuart.lacy@york.ac.uk>
2022-01-04 17:17:36 +0000

    Note to change logging prefix
    Since syslog adds its own prefix, a lot of the information added in the
    logging here is redundant and can be removed once cross-reference and
    also confirmed that the archiving log is visible in syslog.

--- bocs_control/__init__.py
+++ bocs_control/__init__.py
@@ -1,5 +1,6 @@
 import logging
 
+# TODO remove duplicated elements that are in syslog
 logging.basicConfig(
     format="[%(asctime)s.%(msecs)03d] %(levelname)s: %(filename)s:%(lineno)d %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
